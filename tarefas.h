16b167e4bc4d48198774e5cd2f7d6859e2155fe7
Matheus Eleutério <100217154+matheus-eleuterio@users.noreply.github.com>
2024-04-26 14:21:23 +0000

    ENUM para tratamento de erros

--- tarefas.h
+++ tarefas.h
@@ -1,10 +1,37 @@
 #ifndef TAREFAS_H
 #define TAREFAS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CONTATOS 255
+
+typedef enum {
+  OK,
+  ERRO_MAX_CONTATOS,
+  ERRO_SEM_CONTATOS,
+  ERRO_NAO_ENCONTRADO,
+  ERRO_ABRIR,
+  ERRO_FECHAR,
+  ERRO_ESCREVER,
+  ERRO_LER
+} ERROS;
+
+struct Contato {
+  char nome[20];
+  char sobrenome[40];
+  char email[80];
+  char telefone[11];
+};
+
+extern struct Contato agenda[MAX_CONTATOS];
+extern int qnt_contatos;
+
 void adicionar_contato();
 void listar_contatos();
 void deletar_contato();
 void salvar_agenda();
-void carregar_agenda();
+ERROS carregar_agenda();
 
 #endif
